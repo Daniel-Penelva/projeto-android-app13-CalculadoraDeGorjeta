2889a6b40db617ef87fedf1394305ff35280382a
Daniel Penelva <d4n.andrade@gmail.com>
2023-04-14 20:53:21 -0300

    Implementando calculadora gorjeta

--- app/src/main/java/com/formacaoandroidstudio/calculadoradegorjeta/MainActivity.java
+++ app/src/main/java/com/formacaoandroidstudio/calculadoradegorjeta/MainActivity.java
@@ -3,15 +3,92 @@ package com.formacaoandroidstudio.calculadoradegorjeta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    /* criando os atributos dos componentes */
+    private EditText editValor;
+    private TextView textPorcentagem, textGorjeta, textTotal;
+    private SeekBar seekBarGorjeta;
+
+    /* Definir o valor da porcentagem */
+    private double porcentagem = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        /* Capturando os ids dos componentes */
+        editValor = findViewById(R.id.editValor);
+        textPorcentagem = findViewById(R.id.textPorcentagem);
+        textGorjeta = findViewById(R.id.textGorjeta);
+        textTotal = findViewById(R.id.textTotal);
+        seekBarGorjeta = findViewById(R.id.seekBarGorjeta);
+
+        // Adicionar um listener no seekBarGorjeta
+        seekBarGorjeta.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+
+                // Vai receber o progress que define o valor do progresso da barra.
+                porcentagem = progress;
+
+                //Vamos usar a Class Math e afunção round para arredondar o valor de porcentagem para um número sem vírgula.
+                textPorcentagem.setText(Math.round(porcentagem) + " %");
+
+                calcular();
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    /* Método Calcular */
+    public void calcular(){
+
+        // Recupera e converte o valor do editText para uma String
+        String valorRecuperado = editValor.getText().toString();
+
+        // Verifica se o usuário digitou no campo de texto
+        if(valorRecuperado == null || valorRecuperado.equals("")){
+
+            Toast.makeText(getApplicationContext(), "Digite o valor primeiro", Toast.LENGTH_LONG).show();
+
+        }else{
+            // Converter String para Double do valor do campo EditText.
+            double valorDigitado = Double.parseDouble(valorRecuperado);
+
+            // Calcular a gorjeta
+           double gorjeta = valorDigitado * (porcentagem/100);
+
+           // Atribuir o valor no campo de texto da gorjeta
+            textGorjeta.setText("R$ " + Math.round(gorjeta));
+
+            // Calcular total
+            double total = valorDigitado + gorjeta;
+
+            // Atribuir o valor no campo de texto do total
+            textTotal.setText("R$ " + total);
+        }
     }
 }
+
 
 /**
  * Anotações - Layout
@@ -31,7 +108,8 @@ public class MainActivity extends AppCompatActivity {
  * Colocar um layout_width de 70dp e textSize 16dp. Copia e cola duas vezes e troca o text para 'Gorjeta' e para 'Total'.
  * Para 'R$ 0.00' foi feito um background.
  *
- * 4º SeekBar. Trocar de wrap_content para Match constraints (configuração feita no Constraint Widget).
+ * 4º SeekBar. Trocar de wrap_content para Match constraints (configuração feita no Constraint Widget). Em 'max' coloque
+ * o valor 100.
  *
  * Ligar as conexões dos componentes.
  *
